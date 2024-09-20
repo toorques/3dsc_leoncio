eebd6da0e9c6436757b31a5820872130d72e011d
elliott <130366417+toorques@users.noreply.github.com>
2024-09-20 12:05:37 -0300

    Add files via upload

---
+++ whileSetInterval/index.js
@@ -0,0 +1,23 @@
+const { randomBytes } = require("crypto");
+
+// While é um loop sem pausas
+// let n = 0
+// while(n <=5) {
+//   console.log(n);
+//   n = n + 1;
+// }
+
+// enquanto o SetInterval é um loop com pausas.
+let fps = 0;
+setInterval(function(){
+  // fps = math.random(30,60) * 60).toFixed(1)
+  fps = Math.round((Math.random(30,60) * 60))
+  console.clear()
+  console.log(`FPS: ${fps}`);
+}, 1000); // 1 segundo em milisegundos
+
+setInterval(function(){
+  const relogio = new Date().toLocaleTimeString();
+  console.clear();
+  console.log(relogio);
+}, 1000)
